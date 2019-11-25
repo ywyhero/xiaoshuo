7ffcea625a8e7a10a770cbe898584969144eb700
yaoweiyu <yaoweiyu@peilian.com>
2019-11-25 19:00:43 +0800

    优化

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,11 +1,10 @@
-const htp = location.protocol;
-console.log(htp)
+
 const CONFIG = {
     LOCAL: {
         api: 'http://192.168.44.96:3000/api/story',
     },
     PROD: {
-        api: `${htp}//www.vinekan.com:3000/api/story`,
+        api: 'https//www.vinekan.com:3000/api/story',
     },
 };
 interface ConfigObj {
